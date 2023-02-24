7cd461fded9ad9bbeb7192b8aafff13b6d38929e
matheusmota3242 <matheusmota3242@gmail.com>
2023-02-24 14:16:52 -0300

    Primeiro commit

---
+++ src/main/java/br/mmgg/craneapi/CraneApiApplication.java
@@ -0,0 +1,13 @@
+package br.mmgg.craneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraneApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/mmgg/craneapi/CraneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mmgg.craneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
